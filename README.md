ee775e394517ebf05906864c26606cb1b4213493
hexfloo <shoham.palmer@gmail.com>
2022-10-18 14:33:59 +0300

    Add README file, minor cosmetic changes in Main.java

---
+++ README.md
@@ -0,0 +1,2 @@
+## Mortgage Calculator
+This basic command line app can calculate mortgage amount based on user input (principal amount, mortgage rate, and years to pay).
--- src/com/hexfloo/Main.java
+++ src/com/hexfloo/Main.java
@@ -4,7 +4,6 @@ import java.text.NumberFormat;
 import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
         final byte MONTHS_IN_YEAR = 12;
         final byte PERCENT = 100;
