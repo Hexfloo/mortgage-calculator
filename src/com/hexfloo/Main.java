56cf92dc70cf74c0980c4b5f95916ddfe9f38e8e
hexfloo <shoham.palmer@gmail.com>
2022-10-18 14:20:05 +0300

    Add initial files

---
+++ src/com/hexfloo/Main.java
@@ -0,0 +1,68 @@
+package com.hexfloo;
+
+import java.text.NumberFormat;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        final byte MONTHS_IN_YEAR = 12;
+        final byte PERCENT = 100;
+
+        int principalAmount;
+        float annualInterest;
+        byte years;
+        int numOfPayments;
+
+        // Set up scanner
+        Scanner mortgageScanner = new Scanner(System.in);
+
+        // Get Principal amount
+        while (true) {
+            System.out.print("Principal ($1K - $1M): ");
+            principalAmount = mortgageScanner.nextInt();
+            if (principalAmount >= 1_000 && principalAmount <= 1_000_000) {
+                break;
+            } else if (principalAmount < 1_000 || principalAmount > 1_000_000) {
+                System.out.println("Must be a number between 1,000 and 1,000,000");
+            }
+        }
+
+        // Get interest rate and calculate monthly interest
+        while (true) {
+            System.out.print("Annual Interest Rate (%): ");
+            annualInterest = mortgageScanner.nextFloat();
+            if (annualInterest >= 0 && annualInterest <= 30) {
+                break;
+            } else if (annualInterest < 0 || annualInterest > 30) {
+                System.out.println("Enter a value between 0 and 30");
+            }
+        }
+        float monthlyInterestPercent = annualInterest / PERCENT / MONTHS_IN_YEAR;
+
+        // Get period in years
+        while (true) {
+            System.out.print("Period (Years): ");
+            years = mortgageScanner.nextByte();
+            if (years >= 1 && years <= 30) {
+                numOfPayments = years * MONTHS_IN_YEAR;
+                break;
+            } else if (years < 1 || years > 30) {
+                System.out.println("Enter a value between 1 and 30");
+            }
+        }
+
+        // Calculating mortgage
+        double interestPowed = Math.pow((1+monthlyInterestPercent), numOfPayments);
+        double mortgage = principalAmount
+                * ((monthlyInterestPercent * interestPowed)
+                / (interestPowed - 1));
+
+        // Format to currency
+        String result = NumberFormat.getCurrencyInstance().format(mortgage);
+        System.out.println("Mortgage: " + result);
+    }
+}
+
+
+
---
+++ src/com/hexfloo/operatorsPractice.java
@@ -0,0 +1,73 @@
+package com.hexfloo;
+
+import java.util.Scanner;
+
+public class operatorsPractice {
+
+    // For Each loop
+    public static void main(String[] args) {
+        String[] fruits = {"Apple", "Mango", "Orange"};
+
+       for (String fruit : fruits) {
+           System.out.println(fruit.toUpperCase());
+       }
+    }
+
+    /*
+    // FizzBuzz
+    public static void main(String[] args) {
+        Scanner fizzbuzzScanner = new Scanner(System.in);
+        System.out.print("Number: ");
+        int inputNumber = fizzbuzzScanner.nextInt();
+
+        if (inputNumber % 5 == 0 && inputNumber % 3 == 0) {
+            System.out.println("FizzBuzz");
+        } else if (inputNumber % 5 == 0) {
+            System.out.print("Fizz");
+        } else if (inputNumber % 3 == 0) {
+            System.out.println("Buzz");
+        } else {
+            System.out.println(inputNumber);
+        }
+    } */
+
+    /*
+    // Switch-Case statement
+    public static void main(String[] args) {
+        String role = "moderator";
+
+        switch (role) {
+            case "admin":
+                System.out.println("Welcome Admin!");
+                break;
+            case "moderator":
+                System.out.println("Welcome Moderator!");
+                break;
+            default:
+                System.out.println("You are a guest");
+        }
+    } */
+
+    /*
+    // Command line args and ternary operator
+    public static void main(String[] args) {
+        int income = Integer.parseInt(args[0]);
+        String hasHighIncome = income > 100_000 ? "high income" : "not high income";
+        System.out.println(hasHighIncome);
+    } */
+
+    /*
+    // Command line args
+    public static void main(String[] args) {
+        int x = Integer.parseInt(args[0]);
+        int y = Integer.parseInt(args[1]);
+        if (x < y) {
+            System.out.println("x bigger than y");
+        } else {
+            System.out.println("y bigger than x");
+        }
+        for (int i = 0; i < args.length; i++) {
+            System.out.print(args[i] + "\n");
+        }
+    } */
+}
